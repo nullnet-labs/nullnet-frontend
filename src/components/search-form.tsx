f0281ed59c6591b283f7e3d0ece7b249746de57c
hernandezn <hernandezn@live.com>
2026-05-18 22:30:54 -0400

    Added action to search bar; it now submits search-bar input to the /posts page with a URL query parameter

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,19 +1,26 @@
 
+
+
 export default function SearchForm() {
+
+
     return (
-        <form className="mx-auto my-6 flex items-center" style={{width: 'calc(100% - var(--spacing) * 10)'}}>
-            <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-btn cursor-pointer">
-                Search
-            </button>
-            <input
-                type="text"
-                placeholder="Search example: fansite no_ads -is_storefront"
-                className="px-2 py-3 mx-3 md:px-2 md:py-1 md:mx-1 accent-bordered-btn flex-grow-1"
-                style={{boxShadow: '0 0 12px var(--primary-accent)'}}
-            />
-            <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-btn cursor-pointer">
-                Search
-            </button>
-        </form>
+        <search className="mx-auto my-6" style={{ width: 'calc(100% - var(--spacing) * 10)' }}>
+            <form action="posts" className="flex items-center">
+                <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-btn cursor-pointer">
+                    Search
+                </button>
+                <input
+                    type="search"
+                    name="q"
+                    placeholder="Search example: fansite no_ads -is_storefront"
+                    className="px-2 py-3 mx-3 md:px-2 md:py-1 md:mx-1 accent-bordered-btn flex-grow-1"
+                    style={{boxShadow: '0 0 12px var(--primary-accent)'}}
+                />
+                <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-btn cursor-pointer">
+                    Search
+                </button>
+            </form>
+        </search>
     );
 }
