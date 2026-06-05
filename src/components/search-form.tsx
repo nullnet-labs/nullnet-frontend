2a9df4567ea5fa3aabcea6ed1c7b2c9709af4bb2
hernandezn <hernandezn@live.com>
2026-06-05 15:14:48 -0400

    Updated remaining hover-btn classnames to hover-invert; less confusing when hover-glow buttons also exist

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -7,7 +7,7 @@ export default function SearchForm() {
     return (
         <search className="mx-auto my-6" style={{ width: 'calc(100% - var(--spacing) * 10)' }}>
             <form action="posts" className="flex items-center">
-                <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-btn cursor-pointer">
+                <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-invert cursor-pointer">
                     Search
                 </button>
                 <input
@@ -17,7 +17,7 @@ export default function SearchForm() {
                     className="px-2 py-3 mx-3 md:px-2 md:py-1 md:mx-1 accent-bordered-btn flex-grow-1"
                     style={{boxShadow: '0 0 12px var(--primary-accent)'}}
                 />
-                <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-btn cursor-pointer">
+                <button type="submit" className="px-6 py-3 md:px-4 md:py-1 accent-bordered-btn hover-invert cursor-pointer">
                     Search
                 </button>
             </form>
