81968cc6e2ffb7d922ce728c6241dc9ab22ec55f
hernandezn <hernandezn@live.com>
2026-06-13 13:21:29 -0400

    Update page.tsx

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -5,7 +5,7 @@ import styles from '@/component-styles/site-footer.module.css';
 export default function SiteFooter() {
     return (
         <footer className={classes('flex items-center justify-center text-center text-sm min-h-[20dvh]', styles['site-footer'])}>
-            <span>Project Nullnet © Nicolas Hernandez 2026+</span>
+            <span>The Null Network © Nicolas Hernandez 2026+</span>
         </footer>
     )
 }
